4da4c340e4f8cf88f61f4840562acb3704020fef
kristijanmit <kristijan1mit@gmail.com>
2025-04-14 22:30:39 +0200

    added key partners

--- src/screens/sections/HomeSection.js
+++ src/screens/sections/HomeSection.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Background from "../../assets/videos/HomeBckg.mp4";
+import NtpLogo from "../../assets/images/ntpbgLogo.jpg";
+import HubspotLogo from "../../assets/images/hubspot_solutions_logo.png";
 
 const HomeSection = ({ data }) => {
   const handleScrollToContact = (e) => {
@@ -28,29 +30,44 @@ const HomeSection = ({ data }) => {
         <source src={Background} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className="home-container-wrapper">
 
-      <div className="home-container">
-        <h1 className="home-title">{data?.home?.title}</h1>
-        <div className="home-divider"></div>
-        <h3 className="home-subtitle">{data?.home?.subtitle}</h3>
+        <div className="home-container">
+          <h1 className="home-title">{data?.home?.title}</h1>
+          <div className="home-divider"></div>
+          <h3 className="home-subtitle">{data?.home?.subtitle}</h3>
 
-        {/* Trending Section */}
-        <div className="home-texts">
-          <p className="home-text home-trending-title">{data?.home?.text1}</p>
-          <ul className="home-text-list">
-            {trendingTexts.map(([key, value]) => (
-              <li key={key} className="home-text-item">
-                {value}
-              </li>
-            ))}
-          </ul>
+          {/* Trending Section */}
+          <div className="home-texts">
+            <p className="home-text home-trending-title">{data?.home?.text1}</p>
+            <ul className="home-text-list">
+              {trendingTexts.map(([key, value]) => (
+                <li key={key} className="home-text-item">
+                  {value}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <p className="home-subtext">{data?.home?.subtext}</p>
+
+          <a href="/#contact" onClick={handleScrollToContact}>
+            <button className="home-button">{data?.home?.button}</button>
+          </a>
         </div>
+        <div className="partners-container">
+          <p className="partners-title">{data?.home?.logoText}</p>
 
-        <p className="home-subtext">{data?.home?.subtext}</p>
+          <div className="row">
 
-        <a href="/#contact" onClick={handleScrollToContact}>
-          <button className="home-button">{data?.home?.button}</button>
-        </a>
+            <a href="https://ecosystem.hubspot.com/marketplace/solutions/universe-force-group" className="partners-logo">
+              <img src={HubspotLogo} alt="Hubspot Logo" />
+            </a>
+            <a href="https://ntpark.rs/" className="partners-logo">
+              <img src={NtpLogo} alt="Ntp Logo" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
